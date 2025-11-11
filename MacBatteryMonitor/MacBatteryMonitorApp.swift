7e25abd25027d32d1d53b6f084f79770e34b7b72
Rahul Ravi Prakash <eminentrahul@rocketmail.com>
2025-11-11 22:24:04 +0530

    Initial Commit

---
+++ MacBatteryMonitor/MacBatteryMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacBatteryMonitorApp.swift
+//  MacBatteryMonitor
+//
+//  Created by Rahul Ravi Prakash on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MacBatteryMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
